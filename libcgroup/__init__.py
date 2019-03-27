4021e5f8622b1645e66c2fa9d635c48ec7735c35
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-03-27 17:22:16 +0900

    add missing import forwarding to libcgroup package

--- libcgroup/__init__.py
+++ libcgroup/__init__.py
@@ -4,6 +4,8 @@ from libcgroup_bind.init import cgroup_init
 
 # noinspection PyUnresolvedReferences
 from .cgroup import CGroup
+# noinspection PyUnresolvedReferences
+from .mount_point import MountPoint
 from .tools import _raise_error
 
 ret = cgroup_init()
